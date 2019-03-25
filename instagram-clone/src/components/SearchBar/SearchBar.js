9a95560c637ac83672145b0660586544fcf45daf
Duc Vu <de.yd315@gmail.com>
2019-03-25 16:19:01 -0500

    search bar created

---
+++ instagram-clone/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+return (
+    <nav>
+        <span>
+            Logo
+        </span>
+
+        <input>
+            Search
+        </input>
+
+        <span>
+            <a>Feed</a>
+            <a>Likes</a>
+            <a>Profile</a>
+        </span>
+    </nav>
+);
+
+export default SearchBar;
